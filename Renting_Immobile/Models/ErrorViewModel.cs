e13300a6d88f5bd323a44597634f82d2b25f2c6a
hassanrachdi.90 <hassanrachdi.90@gmail.com>
2024-03-24 03:38:56 +0000

    Ajoutez des fichiers projet.

---
+++ Renting_Immobile/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Renting_Immobile.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
