3691d48caf6c797c443c8b77116fb9123fa27979
hassanrachdi.90 <hassanrachdi.90@gmail.com>
2024-03-30 02:59:20 +0000

    30/03

--- Renting.Domain/Entities/Villa.cs
+++ Renting.Domain/Entities/Villa.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,11 +10,16 @@ namespace Renting.Domain.Entities
     public class Villa
     {
         public int Id { get; set; }
+        [MaxLength(50)]
         public string Name { get; set; }
         public string? Description { get; set; }
+        [Display(Name = "Price")] //Annotation
+        [Range(10,10000)]
         public double Price { get; set; }
+        [Range(1, 10)]
         public int Sqft { get; set; }
-        public int Occupancy { get; set; } 
+        public int Occupancy { get; set; }
+        [Display(Name="Image Url")]
         public string? ImageUrl { get; set; }
         public DateTime? CreateDate { get; set; }
         public DateTime? UpdateDate { get; set; }
---
+++ Renting_Immobile/Controllers/VillaController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Renting.Domain.Entities;
+using Renting.Infrastructure.Data;
+
+namespace Renting_Immobile.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public VillaController(ApplicationDbContext db)
+        {
+            _db=db;
+        }
+        public IActionResult Index()
+        {
+            var villas = _db.Villas.ToList();
+            return View(villas);
+        }
+        public IActionResult Create()
+        {  
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Villa obj)
+        {
+            if(obj.Name == obj.Description) 
+            {
+                ModelState.AddModelError("name", "The Description cannot exactly match the name .");
+            
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Villas.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+                
+            }
+            return View();
+            
+        }
+    }
+}
