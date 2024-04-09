b678751116746a3fafddbc5f6f9fa7a3265563a6
hassanrachdi.90 <hassanrachdi.90@gmail.com>
2024-04-09 04:55:59 +0000

    09/04

--- Renting_Immobile/Controllers/HomeController.cs
+++ Renting_Immobile/Controllers/HomeController.cs
@@ -26,7 +26,40 @@ namespace Renting_Immobile.Controllers
             };
             return View(homeVM);
         }
+        [HttpPost]
+        public IActionResult Index(HomeVM homeVM)
+        {
+            homeVM.VillaList = _unitOfWork.Villa.GetAll(includeProperties: "VillaAmenity");
+            foreach(var villa in homeVM.VillaList)
+            {
+                if (villa.Id % 2== 0)
+                {
+                    villa.IsAvailable = false;
+                }
+            }
+            return View(homeVM);
+        }
 
+        public IActionResult GetVillasByDate(int nights, DateOnly checkInDate)
+        {
+            //Thread.Sleep(2000);
+            var villaList= _unitOfWork.Villa.GetAll(includeProperties: "VillaAmenity").ToList();
+            foreach (var villa in villaList)
+            {
+                if (villa.Id % 2 == 0)
+                {
+                    villa.IsAvailable = false;
+                }
+            }
+            HomeVM homeVM = new()
+            {
+                CheckInDate = checkInDate,
+                VillaList = villaList,
+                Nights = nights,
+               
+            };
+            return PartialView("_VillaList",homeVM );
+        }
         public IActionResult Privacy()
         {
             return View();
